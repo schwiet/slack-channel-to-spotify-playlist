e201862d7fb2f93382f0a847d6de55ac2208a700
schwiet <seth.schwiet@pm.me>
2022-12-30 23:25:46 -0800

    refactored code that calls Spotify token endpoint and parses response out of lambda package so it can be reused

--- pkg/lambda/spotify-authorize-callback.go
+++ pkg/lambda/spotify-authorize-callback.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/schwiet/slack-spotify/spotify"
-	"io/ioutil"
-	"net/http"
 	"net/url"
 	"os"
-	"time"
 )
 
 var cbURL url.URL = url.URL{
@@ -26,7 +23,11 @@ func main() {
 	lambda.Start(AuthorizeCallbackHandler)
 }
 
-func AuthorizeCallbackHandler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func AuthorizeCallbackHandler(
+	ctx context.Context, req events.APIGatewayProxyRequest,
+) (
+	events.APIGatewayProxyResponse, error,
+) {
 	clientId, cid_ok := os.LookupEnv("SPOTIFY_CLIENT_ID")
 	clientSecret, cs_ok := os.LookupEnv("SPOTIFY_CLIENT_SECRET")
 
@@ -63,50 +64,21 @@ func AuthorizeCallbackHandler(ctx context.Context, req events.APIGatewayProxyReq
 
 	// TODO: validate state
 
-	tokenPost, err := spotify.GetTokenRequest(
+	token, errResp := spotify.GetAuthToken(
 		code,
 		AUTH_CB_URI,
 		clientId,
 		clientSecret,
 	)
-	if err != nil {
+	if errResp != nil {
 		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       err.Error(),
-		}, nil
-	}
-
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
-	response, err := client.Do(tokenPost)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 502,
-			Body:       err.Error(),
-		}, nil
-	}
-	defer response.Body.Close()
-
-	respBody, err := ioutil.ReadAll(response.Body)
-	var tokenResp tokenResponse
-	err = json.Unmarshal(respBody, &tokenResp)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 502,
-			Body:       err.Error(),
+			StatusCode: errResp.StatusCode,
+			Body:       errResp.Body,
 		}, nil
 	}
 
 	// TODO: store AccessToken, RefreshToken and expiration date
+	fmt.Println(token)
 
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
-}
-
-type tokenResponse struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	Scope        string `json:"scope"`
-	ExpiresIn    int    `json:"expires_in"`
-	RefreshToken string `json:"refresh_token"`
 }
--- pkg/spotify/authorization-token-request.go
+++ pkg/spotify/authorization-token-request.go
@@ -2,10 +2,13 @@ package spotify
 
 import (
 	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // https://developer.spotify.com/documentation/general/guides/authorization/code-flow/#request-access-token
@@ -38,4 +41,56 @@ func GetTokenRequest(
 	tokenPost.Header.Add("Content-Length", strconv.Itoa(len(encodedRequest)))
 
 	return tokenPost, nil
+}
+
+func GetAuthToken(
+	code, redirectUrl, clientId, clientSecret string,
+) (
+	*TokenResponse, AuthTokenError,
+) {
+	tokenPost, err := GetTokenRequest(
+		code,
+		redirectUrl,
+		clientId,
+		clientSecret,
+	)
+	if err != nil {
+		return nil, authTokenErr(500, err.Error())
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	response, err := client.Do(tokenPost)
+	if err != nil {
+		return nil, authTokenErr(502, err.Error())
+	}
+	defer response.Body.Close()
+
+	respBody, err := ioutil.ReadAll(response.Body)
+	var tokenResp *TokenResponse
+	err = json.Unmarshal(respBody, tokenResp)
+	if err != nil {
+		return tokenResp, authTokenErr(502, err.Error())
+	}
+
+	return tokenResp, nil
+}
+
+type authTokenResponse struct {
+	StatusCode int
+	Body       string
+}
+type AuthTokenError *authTokenResponse
+
+func authTokenErr(statusCode int, body string) AuthTokenError {
+	return &authTokenResponse{StatusCode: statusCode, Body: body}
+}
+
+type TokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	Scope        string `json:"scope"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
 }
